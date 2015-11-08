2be14ea5fa038368f94811cff88c26d84a37e3c2
Monilito Castro <monilito.castro@my.und.edu>
2015-11-07 19:20:19 -0600

    Lexer not static. TODO create one-by-one just with global source object.

--- test.java
+++ test.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 public class test{
 	public static void main(String[] args){
-		Lexer lex = new Lexer("test.pas");
-
+		Lexer lex = new Lexer();
+		lex.init("test.pas");
 		while(lex.hasNext()){
 			System.out.println(lex.nextToken());
 		}
