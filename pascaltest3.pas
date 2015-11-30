486969245619dba90f5322ceaa2a2d61ac4bf18b
mcastro <mcastro@localhost.localdomain>
2015-11-29 22:07:34 -0600

    done for the night. TODO: while statement using some modular assembly code so that if statements can reuse it too.

--- pascaltest3.pas
+++ pascaltest3.pas
@@ -1,5 +1,5 @@
 Program temperatureUnit;
-Var f, result ,a,b,c: Integer;
+Var f, result ,a,b,c,z: Integer;
 Procedure proc1;
   Begin
    Write("Proc 1 called.")
@@ -16,8 +16,10 @@ call proc2;
 Write("Celsius is ", result);
 a :=3;
 b := 1;
-while  (a = 3) do
+while  (z < 33) do
   Begin
   b:=b+1
   End;
+  
+Write("b is ", b);
 End.
